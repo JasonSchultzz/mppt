925e4577c201f2689e2d3e9823a279b8ad530aa8
Jason Schultz <jason.schultz@hotmail.ca>
2024-09-10 11:01:19 -0700

    Added code from tutorial

--- src/mppt/kiethley.py
+++ src/mppt/kiethley.py
@@ -8,5 +8,19 @@
 """a_short_module_description"""
 # ---------------------------------------------------------------------------
 
-from pymeasure.instruments.keithley import keithley2400
+from pymeasure.instruments.keithley import Keithley2400
 
+with Keithley2400("GPIB::1") as keithley:
+
+    keithley.apply_current()                # Sets up to source current
+    keithley.source_current_range = 10e-3   # Sets the source current range to 10 mA
+    keithley.compliance_voltage = 10        # Sets the compliance voltage to 10 V
+    keithley.source_current = 0             # Sets the source current to 0 mA
+    keithley.enable_source()                # Enables the source output
+
+    keithley.measure_voltage()              # Sets up to measure voltage
+
+    keithley.ramp_to_current(5e-3)          # Ramps the current to 5 mA
+    print(keithley.voltage)                 # Prints the voltage in Volts
+
+    keithley.shutdown()                     # Ramps the current to 0 mA and disables output
